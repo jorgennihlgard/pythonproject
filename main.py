eb04bbf71842588ca13ce6695fdcabbea5ac59aa
Jörgen Nihlgård <j.nihlgard@outlook.com>
2018-04-12 21:58:56 +0200

    Almost done

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Hello")
-print("bye bye bye")
+import random
+
+def getrandom():
+    random_nr = random.randint(1,10)
+    return random_nr
+
+
+Correctnr = getrandom()
+print(Correctnr)
+count = 0
+while True:
+    count += 1
+    guess = int(input("Gissa ett nummer mellan 1 och 10\n"))
+    if guess == Correctnr:
+        break
+    else:
+        continue
+print(f"Grattis, numret var {Correctnr} Du behövde {count} gissningar")
