bb37c9ef3f793b5740fcac5884f18b82d9a62607
Jorgen N <j.nihlgard@outlook.com>
2018-04-13 10:23:31 +0200

    final version

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import random
-
-def getrandom():
-    random_nr = random.randint(1,10)
-    return random_nr
+from randim import Rand 
 
 
-Correctnr = getrandom()
-print(Correctnr)
-count = 0
-while True:
-    count += 1
-    guess = int(input("Gissa ett nummer mellan 1 och 10\n"))
-    if guess == Correctnr:
-        break
-    else:
-        continue
-print(f"Grattis, numret var {Correctnr} Du behövde {count} gissningar")
+
+class A:
+    Correctnr = Rand.getrandom()
+    print(Correctnr)
+    count = 0
+    while True:
+        count += 1
+        guess = int(input("Gissa ett nummer mellan 1 och 10\n"))
+        if guess == Correctnr:
+            break
+        else:
+            continue
+    print(f"Grattis, numret var {Correctnr} Du behövde {count} gissningar")
---
+++ randim.py
@@ -0,0 +1,9 @@
+
+import random
+class Rand:
+    
+
+    @staticmethod
+    def  getrandom():
+        random_nr = random.randint(1,10)
+        return random_nr
